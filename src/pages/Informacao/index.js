3bc7c68c754919047ef7b029886579ef0c435af9
matheus <matheusteodoro200@gmail.com>
2024-07-17 23:30:49 -0300

    feat:Novas informacoes adicionadas

--- src/pages/Informacao/index.js
+++ src/pages/Informacao/index.js
@@ -13,19 +13,14 @@ const Index = () => {
             <h3 id="conteudo">Livro do mês:</h3>
             <div id="livro">
                 <h3 id="titulo">
-                    A Biblioteca da <br />
-                    meia-noite
+                    Em Agosto nos vemos
+                    <br />
                 </h3>
                 <p id="descricao">
                     <span>Resumo do livro:</span>
-                    um romance incrível que fala dos infinitos rumos que a vida pode tomar e da busca incessante pelo
-                    rumo certo. Aos 35 anos, Nora Seed é uma mulher cheia de talentos e poucas conquistas. Após ser
-                    demitida e seu gato ser atropelado, Nora vê pouco sentido em sua existência e decide colocar um
-                    ponto final em tudo. Porém, quando se vê na Biblioteca da Meia-Noite, Nora ganha uma oportunidade
-                    única de viver todas as vidas que poderia ter vivido. Neste lugar entre a vida e a morte, e graças à
-                    ajuda de uma velha amiga, Nora pode, finalmente, se mudar para a Austrália, reatar relacionamentos
-                    antigos – ou começar outros –, ser uma estrela do rock, uma glaciologista, uma nadadora olímpica...
-                    enfim, as opções são infinitas.
+                    Em agosto nos vemos gira em torno de uma mulher de quarenta e seis anos, conhecida inicialmente
+                    apenas como AMB, que anualmente pega uma barca para uma pequena ilha para visitar o túmulo de sua
+                    mãe. Este ritual é tanto um ato de amor quanto uma busca por compreensão e paz interior.
                 </p>
             </div>
         </div>
